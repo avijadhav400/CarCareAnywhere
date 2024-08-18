005eb3ad0c6afedc4ed8fd6867ae3d735085794a
avijadhav400 <avijadhav400@gmail.com>
2024-08-18 11:12:47 +0530

    fix: health api

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.listen(PORT, ()=>{
     console.log(`Server is running on PORT ${PORT}`)
 })
 
-app.get('/health', (req, res)=>{
+app.get('/', (req, res)=>{
     res.json({
         success: true,
         message: 'Server is healthy'
